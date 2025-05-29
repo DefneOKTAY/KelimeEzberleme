4570ce28093bdebcd1405c0d9a94f4616d3950ff
Serenay Arguç <serenayarguc741@gmail.com>
2025-05-29 21:24:39 +0300

    Delete DTOs directory

--- DTOs/TestResultDto.cs
+++
@@ -1,9 +0,0 @@
-namespace KullaniciWebApi.DTOs
-{
-    public class TestResultDto
-    {
-        public int UserId { get; set; }
-        public int WordId { get; set; }
-        public bool IsCorrect { get; set; }
-    }
-}
